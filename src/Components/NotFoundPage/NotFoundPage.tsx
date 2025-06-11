d54a9d8e6c95195531b52a3981ff8e6857578d4d
MohamedAbdalhalem <mohamed3ab7alem@gmail.com>
2025-06-11 23:31:19 +0300

    handle the error

--- src/Components/NotFoundPage/NotFoundPage.tsx
+++ src/Components/NotFoundPage/NotFoundPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import notFound from '../../assets/modern-vector-illustration-404-error-page-vector-website-electric-plug-socket-unplugged_599740-712.avif'
 export default function NotFoundPage() {
   return (
